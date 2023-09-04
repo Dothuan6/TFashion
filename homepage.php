126adc9957c26b9f24d637402735cbb4657d5ba2
Dothuan6 <thuann6222@gmail.com>
2023-09-04 10:46:23 +0700

    TFashion

--- homepage.php
+++ homepage.php
@@ -160,6 +160,138 @@
         color: black;
         align-items: center !important;
     }
+
+    .container_1 {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 1000px;
+        height: 600px;
+        padding: 50px;
+        background-color: #f5f5f5;
+        box-shadow: 0 30px 50px #dbdbdb;
+    }
+
+    #slide {
+        width: max-content;
+        margin-top: 50px;
+    }
+
+    .item {
+        width: 200px;
+        height: 300px;
+        background-position: 50% 50%;
+        display: inline-block;
+        transition: 0.5s;
+        background-size: cover;
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        transform: translate(0, -50%);
+        border-radius: 20px;
+        box-shadow: 0 30px 50px #505050;
+    }
+
+    .item:nth-child(1),
+    .item:nth-child(2) {
+        left: 0;
+        top: 0;
+        transform: translate(0, 0);
+        border-radius: 0;
+        width: 100%;
+        height: 100%;
+        box-shadow: none;
+    }
+
+    .item:nth-child(3) {
+        left: 50%;
+    }
+
+    .item:nth-child(4) {
+        left: calc(50% + 220px);
+    }
+
+    .item:nth-child(5) {
+        left: calc(50% + 440px);
+    }
+
+    .item:nth-child(n+6) {
+        left: calc(50% + 660px);
+        opacity: 0;
+    }
+
+    .item .content {
+        position: absolute;
+        top: 50%;
+        left: 100px;
+        width: 300px;
+        text-align: left;
+        padding: 0;
+        color: #eee;
+        transform: translate(0, -50%);
+        display: none;
+        font-family: system-ui;
+    }
+
+    .item:nth-child(2) .content {
+        display: block;
+        z-index: 11111;
+    }
+
+    .item .name {
+        font-size: 40px;
+        font-weight: bold;
+        opacity: 0;
+        animation: showcontent 1s ease-in-out 1 forwards
+    }
+
+    .item .des {
+        margin: 20px 0;
+        opacity: 0;
+        animation: showcontent 1s ease-in-out 0.3s 1 forwards
+    }
+
+    .item button {
+        padding: 10px 20px;
+        border: none;
+        opacity: 0;
+        animation: showcontent 1s ease-in-out 0.6s 1 forwards
+    }
+
+    @keyframes showcontent {
+        from {
+            opacity: 0;
+            transform: translate(0, 100px);
+            filter: blur(33px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translate(0, 0);
+            filter: blur(0);
+        }
+    }
+
+    .buttons {
+        position: absolute;
+        bottom: 30px;
+        z-index: 222222;
+        text-align: center;
+        width: 100%;
+    }
+
+    .buttons button {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 1px solid #555;
+        transition: 0.5s;
+    }
+
+    .buttons button:hover {
+        background-color: #bac383;
+    }
     </style>
 </head>
 
@@ -254,54 +386,55 @@
     <!-- third -->
     <div class="row">
         <div class="py-2 m-0 w-50 col-lg-6 col-md-12">
-            <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                        class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
+            <div class="container">
+                <div id="slide">
+                    <div class="item" style="background-image: url(./images/fashion1.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
+                    <div class="item" style="background-image: url(./images/fashion1.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
+                    <div class="item" style="background-image: url(./images/fashion3.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
+                    <div class="item" style="background-image: url(./images/fashion4.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
+                    <div class="item" style="background-image: url(./images/fashion5.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
+                    <div class="item" style="background-image: url(./images/fashion6.jpg);">
+                        <div class="content">
+                            <div class="name">TFashion</div>
+                            <div class="des">Phong cách thời trang quốc tế</div>
+                            <button><a href="all_products.php">See more</a></button>
+                        </div>
+                    </div>
                 </div>
-                <div class="carousel-inner">
-
-                    <div class="carousel-item active">
-                        <img src="./images/fashion1.jpg" class="d-block w-75 m-auto" style="height: 400px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>TFashion Shop - Phong Cách Thời Thượng</h5>
-                            <button class="btn btn-outline btn-success"><a class="text-decoration-none text-light"
-                                    href="./all_products.php">Xem sản phẩm</a></button>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./images/fashion2.jpg" class="d-block w-75 m-auto" style="height: 400px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>TFashion Shop - Phong Cách Thời Thượng</h5>
-                            <button class="btn btn-outline btn-success"><a class="text-decoration-none text-light"
-                                    href="./all_products.php">Xem sản phẩm</a></button>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./images/fashion3.jpg" class="d-block w-75 m-auto" style="height: 400px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>TFashion Shop - Phong Cách Thời Thượng</h5>
-                            <button class="btn btn-outline btn-success"><a class="text-decoration-none text-light"
-                                    href="./all_products.php">Xem sản phẩm</a></button>
-                        </div>
-                    </div>
+                <div class="buttons">
+                    <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
+                    <button id="next"><i class="fa-solid fa-angle-right"></i></button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon text-black" aria-hidden="true"><i
-                            class="fa-solid fa-angles-left"></i></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon text-black" aria-hidden="true"><i
-                            class="fa-solid fa-angles-right"></i></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
         </div>
 
@@ -436,6 +569,16 @@
     /*Hàm Đóng Form*/
     function dongForm() {
         document.getElementById("myForm").style.display = "none";
+    }
+    </script>
+    <script>
+    document.getElementById('next').onclick = function() {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').appendChild(lists[0]);
+    }
+    document.getElementById('prev').onclick = function() {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').prepend(lists[lists.length - 1]);
     }
     </script>
 </body>
