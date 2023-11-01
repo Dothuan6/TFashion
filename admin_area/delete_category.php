bb9ee5ffae3b3196ded3e0d04ca8fc49aecea1a0
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:58:46 +0700

    delete_category

--- admin_area/delete_category.php
+++ admin_area/delete_category.php
@@ -1,8 +1,10 @@
 <?php
+global $conn;
 if(isset($_GET['delete_category'])){
     $get_category_id=$_GET['delete_category'];
-    $delete_category="delete from `categories` where category_id = '$get_category_id'";
-    $result_delete_category=mysqli_query($con,$delete_category);
+    $delete_category="delete from `categories` where category_id = ?";
+    $stmt = $conn->prepare($delete_category);
+    $result_delete_category=$stmt->execute([$get_category_id]);
     if($result_delete_category){
         echo "<script>alert('Xóa thành công!')</script>";
         echo "<script>window.open('./index.php?insert_categories','_self')</script>";
