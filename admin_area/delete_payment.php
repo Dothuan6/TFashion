fdafc6fd4779ef4d0da44c445e4067ed2d5b2f3f
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:59:03 +0700

    delete_payment

--- admin_area/delete_payment.php
+++ admin_area/delete_payment.php
@@ -1,8 +1,11 @@
 <?php
+global $conn;
 if(isset($_GET['delete_payments'])){
     $payment_id = $_GET['delete_payments'];
-    $delete_payments = "delete from `user_payments` where payment_id='$payment_id'";
-    $result_payments=mysqli_query($con,$delete_payments);
+    $delete_payments = "delete from `user_payments` where payment_id=?";
+    $stmt = $conn->prepare($delete_payments);
+   
+    $result_payments= $stmt ->execute([$payment_id]);
     if($result_payments){
         echo "<script>alert('Xóa thành công!')</script>";
         echo "<script>window.open('./index.php?view_payments','_self')</script>";
