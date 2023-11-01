d5d25fcc5df04fe67ee80cff717cfb207bb20417
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:58:35 +0700

    delete_brand

--- admin_area/delete_brand.php
+++ admin_area/delete_brand.php
@@ -1,8 +1,10 @@
 <?php
 if(isset($_GET['delete_brand'])){
+    global $conn;
     $get_brand_id=$_GET['delete_brand'];
-    $delete_brand="delete from `brands` where brand_id = '$get_brand_id'";
-    $result_delete_brand=mysqli_query($con,$delete_brand);
+    $delete_brand="delete from `brands` where brand_id = ?";
+    $stmt = $conn->prepare($delete_brand);
+    $result_delete_brand = $stmt->execute([$get_brand_id]);
     if($result_delete_brand){
         echo "<script>alert('Xóa thành công!')</script>";
         echo "<script>window.open('./index.php?insert_brands','_self')</script>";
