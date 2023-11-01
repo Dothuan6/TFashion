90905fbfb4c4c879e5e33d5afc964d11f4dc801b
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:58:56 +0700

    delete_order

--- admin_area/delete_order.php
+++ admin_area/delete_order.php
@@ -1,8 +1,11 @@
 <?php
+global $conn;
     if(isset($_GET['delete_orders'])){
         $invoice_number = $_GET['delete_orders'];
-        $delete_orders="delete from `user_orders` where invoice_number='$invoice_number'";
-        $result_orders=mysqli_query($con,$delete_orders);
+        $delete_orders="delete from `user_orders` where invoice_number=?";
+        $stmt = $conn->prepare($delete_orders);
+
+        $result_orders= $stmt->execute([$invoice_number]);
         
         if($result_orders){
             echo "<script>alert('Xóa thành công!')</script>";
