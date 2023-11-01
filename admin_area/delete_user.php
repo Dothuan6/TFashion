c9451d2926178a1cb4ae7054160163cce87197aa
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:59:19 +0700

    delete_user

--- admin_area/delete_user.php
+++ admin_area/delete_user.php
@@ -1,8 +1,11 @@
 <?php
+global $conn;
 if(isset($_GET['delete_users'])){
     $user_id = $_GET['delete_users'];
-    $delete_users = "delete from `user_table` where user_id='$user_id'";
-    $result_users=mysqli_query($con,$delete_users);
+    $delete_users = "delete from `user_table` where user_id=?";
+    $stmt = $conn->prepare($delete_users);
+    $result_users= $stmt ->execute([$user_id]);
+
     if($result_users){
         echo "<script>alert('Xóa thành công!')</script>";
         echo "<script>window.open('./index.php?view_users','_self')</script>";
