d725049970fc5fef51aafddf969783ab263a56fd
Dothuan6 <thuann6222@gmail.com>
2023-11-01 15:59:11 +0700

    delete_product

--- admin_area/delete_product.php
+++ admin_area/delete_product.php
@@ -1,8 +1,10 @@
 <?php
+global $conn;
 if(isset($_GET['delete_products'])){
     $delete_id=$_GET['delete_products'];
-    $delete_product="delete from `products` where product_id='$delete_id'";
-    $result_product=mysqli_query($con,$delete_product);
+    $delete_product="delete from `products` where product_id=?";
+    $stmt = $conn->prepare($delete_product);
+    $result_product= $stmt ->execute([$delete_id]);
     if($result_product){
         echo "<script>alert('Xóa thành công!')</script>";
         echo "<script>window.open('./index.php?view_products','_self')</script>";
